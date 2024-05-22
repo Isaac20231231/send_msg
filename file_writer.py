159bea178b11416aad46ec997b220f27a048b9b2
Isaac <135930483+Isaac20231231@users.noreply.github.com>
2024-05-22 10:09:36 +0800

    Add files via upload

---
+++ file_writer.py
@@ -0,0 +1,90 @@
+from plugins import *
+from flask import Flask, request, jsonify
+import json
+from common.log import logger
+from plugins import Plugin, register
+import threading
+
+app = Flask(__name__)
+
+
+def validate_data(data_list):
+    # 检查data_list是否为列表类型
+    if not isinstance(data_list, list):
+        raise ValueError('data_list必须为列表类型')
+    # 检查data_list是否为空
+    if not data_list:
+        raise ValueError('data_list不能为空')
+    # 遍历data_list,检查每个元素是否包含必要的字段
+    for data in data_list:
+        if not isinstance(data, dict):
+            raise ValueError('data_list的每个元素必须为字典类型')
+        if 'message' not in data:
+            raise ValueError('每个消息必须包含message')
+
+
+@register(
+    name="file_writer",
+    desire_priority=120,
+    hidden=True,
+    desc="写入文件api",
+    version="1.0",
+    author="Isaac",
+)
+class FileWriter(Plugin):
+    def __init__(self):
+        super().__init__()
+        try:
+            self.conf = super().load_config()
+            if not self.conf:
+                curdir = os.path.dirname(__file__)
+                # 配置文件路径
+                config_path = os.path.join(curdir, "data.json")
+
+                # 加载或创建配置文件
+                if os.path.exists(config_path):
+                    with open(config_path, "r") as f:
+                        self.conf = json.load(f)
+                else:
+                    self.conf = {}
+                    with open(config_path, "w") as f:
+                        json.dump(self.conf, f, indent=4)
+        except Exception as e:
+            logger.warn("[file_writer] init failed, ignore.")
+            raise e
+
+    @staticmethod
+    @app.route('/send_message', methods=['POST'])
+    def send_message():
+        try:
+            # 获取请求中的数据
+            data_list = request.json.get('data_list', [])  # 获取消息列表
+            try:
+                validate_data(data_list)
+            except ValueError as e:
+                return jsonify({'status': 'error', 'message': str(e)}), 400
+
+            curdir = os.path.dirname(__file__)
+            # 配置文件路径
+            config_path = os.path.join(curdir, "data.json")
+            # 将数据写入到message.json文件中
+            with open(config_path, 'w') as file:
+                json.dump(data_list, file, ensure_ascii=False)
+            logger.info(f"写入成功,写入内容{data_list}")
+
+            return jsonify({'status': 'success', 'message': '发送成功'}), 200
+        except Exception as e:
+            logger.error(f"处理请求时发生错误: {str(e)}")
+            return jsonify({'status': 'error', 'message': '服务器内部错误'}), 500
+
+    def get_help_text(self, **kwargs):
+        return "写入文件api插件"
+
+    # 启动Flask服务器的函数
+    @staticmethod
+    def run_flask_app():
+        app.run(host='0.0.0.0', port=8899, debug=False)
+
+    # 在单独的线程中启动Flask服务器
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.start()
