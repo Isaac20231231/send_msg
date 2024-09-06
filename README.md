9acabe5a896f04e941fbedc3f8984b45ff8c7b76
isaac <969408250@qq.com>
2024-09-06 10:37:31 +0800

    2.3版本发布，兼容ntchat框架客户端微信发送消息

--- README.md
+++ README.md
@@ -158,6 +158,9 @@ send_msg支持以下命令(支持一次性发多人,单人时列表只填一个�
   <img src="微信命令发送消息成功示例.png" width="600">
 
 ## 更新日志
+### V2.3 （2024-09-05）
+- 1.增加channel判断，兼容win版本机器人的ntchat发送消息（https://github.com/Tishon1532/chatgpt-on-wechat-win），
+- 支持itchat和ntchat两种channel类型，注意ntchat目前还没解决群聊@所有人的场景，只能@单个人，另外ntchat的receiver_name只支持填写微信名字
 
 ### V2.2 （2024-08-15）
 - 修改了插件名字和文件夹一致
--- send_msg.py
+++ send_msg.py
@@ -1,3 +1,35 @@
+"""
+发送消息到微信的插件
+@Version: 2.3
+@Description: 实现了一个监听文件变化发送消息的插件，可以通过微信命令/api接口请求 发送消息到微信
+@Author: Isaac
+@Date: 2024-05-24
+@Update: 2024-09-05
+@更新日志:
+    2024-09-05: 2.3版本
+        1.增加channel判断，兼容win版本机器人的ntchat发送消息（https://github.com/Tishon1532/chatgpt-on-wechat-win），
+        支持itchat和ntchat两种channel类型，注意ntchat目前还没解决群聊@所有人的场景，只能@单个人，另外ntchat的receiver_name只支持填写微信名字
+    2024-08-15: 2.2版本
+        1.修改了插件名字和文件夹一致
+        2.api服务修改逻辑，不注册插件，采用函数调用的方式启动api服务
+        ps:注意因为此次修改了插件名称，之前/plugins/plugins.json目录下的file_writer和file_watcher两个插件名字需要删除
+    2024-07-19: 2.1版本
+        1.优化兼容发送好友消息，先查找微信备注名，找不到再查找微信昵称，receiver_name支持填写微信备注名和微信昵称。
+        2.优化了发送群聊消息的逻辑，之前一定要加好友才能@指定人，现在不需要加好友也可以@指定人。
+        （先从群聊里找微信名，找不到通过好友列表找微信备注名，备注名没有再找微信昵称）
+    2024-07-18: 2.0版本
+        1.新增支持发送图片、视频和文件的消息格式（文件内容传参http或者https的url）
+    2024-05-29: 1.5版本，
+        1.新增支持微信命令发送消息功能。
+        2.优化了文件监听的模式，使用了python看门狗模式监听文件变化
+        3.更新了文档，添加了更多的使用示例。
+        4.修复了一些已知的 bug。
+    2024-05-24: 1.0版本
+        1.初始插件版本发布，支持基本api触发消息发送微信功能。
+        2.提供了简单的配置选项和说明文档。
+@联系作者：微信号：shine86869 （备注ai插件）
+"""
+
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -5,8 +37,8 @@ from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
 from plugins import *
 import plugins
-from lib import itchat
 from plugins.send_msg.file_api import FileWriter
+from config import conf
 
 
 class FileChangeHandler(FileSystemEventHandler):
@@ -23,20 +55,37 @@ class FileChangeHandler(FileSystemEventHandler):
     desire_priority=180,
     hidden=True,
     desc="watchdog监听文件变化发送消息&微信命令发送消息",
-    version="2.2",
+    version="2.3",
     author="Isaac"
 )
 class FileWatcherPlugin(Plugin):
     def __init__(self):
         super().__init__()
+        self.channel = None
         self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
         curdir = os.path.dirname(__file__)
-        # 启动api服务
-        FileWriter()
+        FileWriter()  # 启动api服务
         self.file_path = os.path.join(curdir, "data.json")
         self.observer = Observer()
         self.event_handler = FileChangeHandler(self.handle_message)
         self.start_watch()  # 默认启动 watchdog 监听
+
+        # 根据配置获取当前的channel类型
+        self.channel_type = conf().get("channel_type", "wx")
+        if self.channel_type == "wx":
+            try:
+                from lib import itchat
+                self.channel = itchat
+            except Exception as e:
+                logger.error(f"未安装itchat: {e}")
+        elif self.channel_type == "ntchat":
+            try:
+                from channel.wechatnt.ntchat_channel import wechatnt
+                self.channel = wechatnt
+            except Exception as e:
+                logger.error(f"未安装ntchat: {e}")
+        else:
+            logger.error(f"不支持的channel_type: {self.channel_type}")
 
     def on_handle_context(self, e_context: EventContext):
         if e_context['context'].type != ContextType.TEXT:
@@ -49,7 +98,6 @@ class FileWatcherPlugin(Plugin):
             reply.type = ReplyType.INFO
             reply.content = "watchdog started."
             e_context['reply'] = reply
-            logger.info("watchdog监听已启动,监听data.json文件变化发送微信通知.")
             self.handle_message()  # 处理文件中的现有数据
         elif content == "$stop watchdog":
             self.stop_watch()
@@ -58,18 +106,16 @@ class FileWatcherPlugin(Plugin):
             reply.type = ReplyType.INFO
             reply.content = "watchdog stopped."
             e_context['reply'] = reply
-            logger.info("watchdog监听已停止.写入文件data.json不再发送微信通知.")
         elif content == "$check watchdog":
             if self.observer.is_alive():
                 reply = Reply()
                 reply.type = ReplyType.INFO
                 reply.content = "watchdog 正在运行,如需停止:关闭命令 $stop watchdog."
-                e_context['reply'] = reply
             else:
                 reply = Reply()
                 reply.type = ReplyType.INFO
                 reply.content = "watchdog 没在运行,如需启动:启动命令 $start watchdog."
-                e_context['reply'] = reply
+            e_context['reply'] = reply
             e_context.action = EventAction.BREAK_PASS
         elif content.startswith("$send_msg"):
             try:
@@ -151,69 +197,34 @@ class FileWatcherPlugin(Plugin):
         except Exception as e:
             logger.error(f"处理消息时发生异常: {e}")
 
-    # 下载文件到本地的函数
-    def download_file(self, url):
-        """
-        下载文件到本地
-        :param url: 文件的URL
-        """
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                # 提取文件名
-                file_name = os.path.basename(url)
-                # 创建临时文件
-                with open(file_name, 'wb') as file:
-                    file.write(response.content)
-                return file_name
-            else:
-                return None
-        except Exception as e:
-            logger.error(f"下载文件时发生异常: {e}")
-            return None
-
-    # 定义发送消息的函数
-    def send_msg(self, msg_type, content, to_user_name, at_content=None):
-        """
-        实际发送消息函数
-        :param msg_type: 消息类型
-        :param content: 消息内容
-        :param to_user_name: 接收者的 UserName
-        :param at_content: @的内容
-        """
-        if msg_type == 'text':
-            if at_content:
-                itchat.send(f'{at_content} {content}', to_user_name)
-            else:
-                itchat.send(content, to_user_name)
-        elif msg_type in ['img', 'video', 'file']:
-            # 如果是图片、视频或文件,先下载到本地
-            local_file_path = self.download_file(content)
-            if local_file_path:
-                itchat.send(at_content, to_user_name)
-                if msg_type == 'img':
-                    itchat.send_image(local_file_path, to_user_name)
-                elif msg_type == 'video':
-                    itchat.send_video(local_file_path, to_user_name)
-                elif msg_type == 'file':
-                    itchat.send_file(local_file_path, to_user_name)
-                # 发送完成后删除本地临时文件
-                os.remove(local_file_path)
-            else:
-                raise ValueError(f"无法下载文件: {content}")
-
     def send_message(self, receiver_names, content, group_names=None):
         """
-        发送消息
+        发送消息，根据channel_type选择使用ntchat或itchat
+        如果type是wx使用itchat，如果是ntchat使用ntchat
         :param receiver_names: 接收者名称列表
         :param content: 消息内容
         :param group_names: 群聊名称列表
         """
+        try:
+            if self.channel_type == "wx":
+                self._send_itchat_message(receiver_names, content, group_names)
+            elif self.channel_type == "ntchat":
+                self._send_ntchat_message(receiver_names, content, group_names)
+            else:
+                raise ValueError(f"不支持的channel_type: {self.channel_type}")
+        except Exception as e:
+            logger.error(f"处理消息时发生异常: {e}")
+            raise e
+
+    def _send_itchat_message(self, receiver_names, content, group_names):
+        """
+        使用 itchat 发送消息的逻辑
+        """
         global media_type, content_at
         try:
             # 更新 itchat 的内部缓存
-            itchat.get_friends(update=True)
-            itchat.get_chatrooms(update=True)
+            self.channel.get_friends(update=True)
+            self.channel.get_chatrooms(update=True)
 
             # 判断消息类型
             if content.startswith(("http://", "https://")):
@@ -230,7 +241,7 @@ class FileWatcherPlugin(Plugin):
 
             if group_names:
                 for group_name in group_names:
-                    chatrooms = itchat.search_chatrooms(name=group_name)
+                    chatrooms = self.channel.search_chatrooms(name=group_name)
                     if not chatrooms:
                         raise ValueError(f"没有找到对应的群聊：{group_name}")
                     chatroom = chatrooms[0]
@@ -248,9 +259,9 @@ class FileWatcherPlugin(Plugin):
                                         member_found = True
                                         break
                                 if not member_found:
-                                    friends = itchat.search_friends(remarkName=receiver_name)
+                                    friends = self.channel.search_friends(remarkName=receiver_name)
                                     if not friends:
-                                        friends = itchat.search_friends(name=receiver_name)
+                                        friends = self.channel.search_friends(name=receiver_name)
                                     if friends:
                                         content_at = f"@{friends[0].NickName} "
                                         member_found = True
@@ -266,9 +277,9 @@ class FileWatcherPlugin(Plugin):
             else:
                 if receiver_names and any(receiver_names):
                     for receiver_name in receiver_names:
-                        friends = itchat.search_friends(remarkName=receiver_name)
+                        friends = self.channel.search_friends(remarkName=receiver_name)
                         if not friends:
-                            friends = itchat.search_friends(name=receiver_name)
+                            friends = self.channel.search_friends(name=receiver_name)
                         if friends:
                             self.send_msg(media_type, content, friends[0].UserName)
                             logger.info(f"手动发送微信消息成功, 发送人:{friends[0].NickName} 消息内容：{content}")
@@ -280,11 +291,187 @@ class FileWatcherPlugin(Plugin):
             logger.error(f"处理消息时发生异常: {e}")
             raise e
 
+    def _send_ntchat_message(self, receiver_names, content, group_names):
+        """
+        使用 ntchat 发送消息的逻辑
+        """
+        try:
+            if content.startswith(("http://", "https://")):
+                if content.lower().endswith((".jpg", ".jpeg", ".png", ".gif", ".img")):
+                    media_type = "img"
+                elif content.lower().endswith((".mp4", ".avi", ".mov", ".pdf")):
+                    media_type = "video"
+                elif content.lower().endswith((".doc", ".docx", ".xls", ".xlsx", ".zip", ".rar", "txt")):
+                    media_type = "file"
+                else:
+                    logger.warning(f"不支持的文件类型: {content}")
+                    return
+            else:
+                media_type = "text"
+
+            # 处理群聊消息
+            if group_names:
+                for group_name in group_names:
+                    rooms = self.channel.get_rooms()  # 获取所有群聊
+                    room_status = False
+
+                    for room in rooms:
+                        if group_name == room.get("nickname"):
+                            room_status = True
+                            logger.info(f"找到对应的群聊: {room}")
+                            wxid = room.get("wxid")
+                            room_members = self.channel.get_room_members(wxid)
+                            logger.info(f"{wxid}, room_members: {room_members}")
+                            user_wxids = []
+
+                            if receiver_names and any(receiver_names):
+                                # 检查是否是@所有人
+                                if "所有人" in receiver_names or "all" in receiver_names:
+                                    logger.info(f"发送群聊消息，@所有人: {content}")
+                                    at_content = f"@所有人 {content}"
+                                    self.channel.send_room_at_msg(wxid, at_content, [])
+                                    logger.info(
+                                        f"成功发送群聊消息, 群聊: {group_name}, 消息: {at_content},接收人: 所有人")
+                                else:
+                                    # 查找群成员并构造 @ 的内容
+                                    for room_member in room_members["member_list"]:
+                                        for receiver_name in receiver_names:
+                                            if room_member.get("nickname") == receiver_name:
+                                                logger.info(f"找到对应的群成员: {receiver_name}")
+                                                user_wxid = room_member.get("wxid")
+                                                user_wxids.append(user_wxid)
+
+                                    member_n = len(receiver_names)
+                                    if len(user_wxids) == member_n:
+                                        # 构造 @ 消息的内容
+                                        at_content = f"{' '.join([f'@{receiver_name}' for receiver_name in receiver_names])} {content}"
+                                        logger.info(f"找到所有对应的群成员，发送消息内容: {at_content}")
+                                        self.channel.send_room_at_msg(wxid, at_content, user_wxids)
+                                        logger.info(
+                                            f"send_room_at_msg: wxid: {wxid}, content: {at_content},user_wxids: {user_wxids}，群聊名称: {group_name}")
+                                    else:
+                                        logger.warning(f"未找到所有指定的成员: {receiver_names}")
+                            else:
+                                # receiver_names为空时发送普通消息
+                                self.channel.send_text(wxid, content)
+                                logger.info(f"发送普通群聊消息，没有@成员: {content}")
+                            break
+
+                    if not room_status:
+                        logger.warning(f"未找到对应的群聊: {group_name}")
+
+            # 处理单聊消息
+            else:
+                for receiver_name in receiver_names:
+                    wxid = self._find_friend_by_name(receiver_name)
+                    if wxid:
+                        self._send_ntchat_media_or_text(media_type, content, wxid)
+                        logger.info(f"成功发送单聊消息, 接收人: {receiver_name}, 消息: {content}")
+                    else:
+                        raise ValueError(f"没有找到对应的好友：{receiver_name}")
+
+        except Exception as e:
+            logger.error(f"发送ntchat消息时发生异常: {e}")
+            raise e
+
+    def _find_chatroom_by_name(self, group_name):
+        """
+        根据群聊名称查找群聊
+        """
+        rooms = self.channel.get_rooms()  # 获取所有群聊
+        for room in rooms:
+            if room["nickname"] == group_name:
+                return room
+        return None
+
+    def _find_member_in_chatroom(self, group_wxid, member_name):
+        """
+        根据成员名称在群聊中查找成员wxid
+        """
+        room_members = self.channel.get_room_members(group_wxid)
+        for member in room_members["member_list"]:
+            if member["nickname"] == member_name:
+                return member["wxid"]
+        return None
+
+    def _find_friend_by_name(self, friend_name):
+        """
+        根据好友名称查找wxid
+        """
+        friends = self.channel.get_contacts()  # 获取所有好友
+        for friend in friends:
+            if friend["nickname"] == friend_name or friend["remark"] == friend_name:
+                return friend["wxid"]
+        return None
+
+    def _send_ntchat_media_or_text(self, media_type, content, wxid):
+        """
+        ntchat根据消息类型发送文本、图片、视频或文件
+        """
+        if media_type == "text":
+            self.channel.send_text(wxid, content)
+        elif media_type == "img":
+            image_path = self.download_file(content)
+            self.channel.send_image(wxid, image_path)
+        elif media_type == "video":
+            video_path = self.download_file(content)
+            self.channel.send_video(wxid, video_path)
+        elif media_type == "file":
+            file_path = self.download_file(content)
+            self.channel.send_file(wxid, file_path)
+        else:
+            logger.error(f"不支持的消息类型: {media_type}")
+
+    def send_msg(self, msg_type, content, to_user_name, at_content=None):
+        """
+        实际itchat发送消息函数
+        :param msg_type: 消息类型
+        :param content: 消息内容
+        :param to_user_name: 接收者的 UserName
+        :param at_content: @的内容
+        """
+        if msg_type == 'text':
+            if at_content:
+                self.channel.send(f'{at_content} {content}', to_user_name)
+            else:
+                self.channel.send(content, to_user_name)
+        elif msg_type in ['img', 'video', 'file']:
+            # 如果是图片、视频或文件,先下载到本地
+            local_file_path = self.download_file(content)
+            if local_file_path:
+                self.channel.send(at_content, to_user_name)
+                if msg_type == 'img':
+                    self.channel.send_image(local_file_path, to_user_name)
+                elif msg_type == 'video':
+                    self.channel.send_video(local_file_path, to_user_name)
+                elif msg_type == 'file':
+                    self.channel.send_file(local_file_path, to_user_name)
+                # 发送完成后删除本地临时文件
+                os.remove(local_file_path)
+            else:
+                raise ValueError(f"无法下载文件: {content}")
+
+    def download_file(self, url):
+        """
+        下载文件到本地
+        :param url: 文件的URL
+        """
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                file_name = os.path.basename(url)
+                with open(file_name, 'wb') as file:
+                    file.write(response.content)
+                return file_name
+            return None
+        except Exception as e:
+            logger.error(f"下载文件时发生异常: {e}")
+            return None
+
     def get_help_text(self, **kwargs):
-        return ("1.watchdog监听文件变化插件,监听data.json文件变化发送微信通知.(默认启动)\n"
+        return ("1. watchdog监听文件变化插件,监听data.json文件变化发送微信通知.(默认启动)\n"
                 "启动监听: $start watchdog\n停止监听: $stop watchdog\n查看监听状态: $check watchdog\n\n"
-                "2.微信命令发送消息: \n"
-                "2.1 发送个人消息: \n$send_msg [微信备注名1,微信备注名2] 消息内容\n"
-                "2.2 发送群聊消息@指定人: \n$send_msg [微信备注名1,微信备注名2] 消息内容 group[群聊名称1,群聊名称2]\n"
-                "2.3 发送群聊消息@所有人: \n$send_msg [所有人] 消息内容 group[群聊名称1,群聊名称2]\n"
-                "2.4 发送群聊消息不@任何人: \n$send_msg [] 消息内容 group[群聊名称1,群聊名称2]")
+                "2. 微信命令发送消息: \n"
+                "$send_msg [微信备注名1,微信备注名2] 消息内容\n"
+                "$send_msg [微信备注名1,微信备注名2] 消息内容 group[群聊名称1,群聊名称2]\n"
+                "$send_msg [所有人] 消息内容 group[群聊名称1,群聊名称2]")
